a800d8a64c8ae0101418fbb5e328f9aae1ed2d85
Andrey Kudishkin <kudyshkin.a.k@mail.ru>
2025-03-28 10:49:13 +0300

    chore: refactor main.py file

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,24 +1,29 @@
 """main файл."""
 from internal.config import read_cfg
 from tables.table_manager import TableManager
+from internal.logger import set_up_logger
 from sql.sql_parser import SQLParser
 from server.server import Server
+from serialization.serializer import Serializer
 
 
 def main():
     """."""
     cfg = read_cfg()
 
+    set_up_logger()
+
+    serializer = Serializer()
+
     sql_parser = SQLParser()
 
-    table_manager = TableManager(cfg)
+    table_manager = TableManager(cfg, serializer=serializer)
 
+    print('starting server...')
     server = Server(
-        protocol_handler=None,
         cfg=cfg,
         sqlparser=sql_parser,
-        tablemanager=table_manager,
-        logger=None
+        tablemanager=table_manager
     )
 
     server.run()
