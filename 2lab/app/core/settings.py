cec27fe49c57b24033ad02d2eab23001f3c37676
Andrey Kudishkin <kudyshkin.a.k@mail.ru>
2025-03-31 13:52:36 +0300

    chore: update settings.py file for .env file

--- 2lab/app/core/settings.py
+++ 2lab/app/core/settings.py
@@ -1,1 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app/db/database.db"
+TOKEN_LIFETIME_MIN = 60
+KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
