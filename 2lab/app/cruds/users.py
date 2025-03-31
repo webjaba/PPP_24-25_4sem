c17027f67f69b3ec6807b82dd88b832eee8901d9
Andrey Kudishkin <kudyshkin.a.k@mail.ru>
2025-03-31 13:08:19 +0300

    feat(crud): make a crud for users table

---
+++ 2lab/app/cruds/users.py
@@ -0,0 +1,49 @@
+from app.schemas import schemas
+from app.models.models import User
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./mydatabase.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+db = SessionLocal()
+
+
+def create_user(user: schemas.User) -> dict:
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if db_user:
+        return {"id": db_user.id, "exists": True}
+
+    db_user = User(email=user.email, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return {"id": db_user.id, "exists": False}
+
+
+def get_user(user: schemas.User) -> dict:
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        return {
+            "id": -1,
+            "email": "",
+            "password": "",
+            "exists": False
+        }
+
+    return {
+        "id": db_user.id,
+        "email": db_user.email,
+        "password": db_user.password,
+        "exists": True
+    }
